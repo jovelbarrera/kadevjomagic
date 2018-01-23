a145b486eff85f09478177345a7508b2f177f56f
Roberto Jovel <rej.barrera@gmail.com>
2018-01-23 16:00:24 -0600

    Stable version 1

--- MagicKadevjo/MagicKadevjoPage.xaml.cs
+++ MagicKadevjo/MagicKadevjoPage.xaml.cs
@@ -11,14 +11,14 @@ namespace MagicKadevjo
         {
             InitializeComponent();
             var tap = new TapGestureRecognizer();
-            //tap.Tapped += async (sender, e) =>
-            //{
-            //    playButton.IsEnabled = true;
-            //    DependencyService.Get<ISoundPlayer>().Play(RandomAudio());
-            //    await Task.Delay(2000);
-            //    playButton.IsEnabled = true;
-            //};
-            //playButton.GestureRecognizers.Add(tap);
+            tap.Tapped += async (sender, e) =>
+            {
+                playButton.IsEnabled = true;
+                DependencyService.Get<ISoundPlayer>().Play(RandomAudio());
+                await Task.Delay(2000);
+                playButton.IsEnabled = true;
+            };
+            playButton.GestureRecognizers.Add(tap);
         }
 
         private string RandomAudio()
@@ -41,7 +41,8 @@ namespace MagicKadevjo
             };
             Random rnd = new Random();
             int index = rnd.Next(audios.Length);
-            return "https://firebasestorage.googleapis.com/v0/b/test-fda21.appspot.com/o/hey_nombe_hey.wav?alt=media&token=4230ede8-f607-4b79-91ca-edda7998f1ff";//audios[index];
+            //return "https://firebasestorage.googleapis.com/v0/b/test-fda21.appspot.com/o/hey_nombe_hey.wav?alt=media&token=4230ede8-f607-4b79-91ca-edda7998f1ff";
+            return audios[index];
         }
     }
 }
